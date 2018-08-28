9d988a6827a0e139801caa230e2f63c7f8400ca0
Morozov Pavel <moroz0@gmail.com>
2018-08-28 12:50:21 +0400

    Update 2018-08-28T08:50:11.557Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,5 +9,19 @@
  * Так же доступа к блокам, которые собираются с помощью вебпака не будет.
  */
 
+$(document).ready(function () {
+  $('.js-catalog').click(function () {
+
+    //$('.container__slider .main').fadeOut();
+    //$('.main-section__content').toggleClass('main-section__content_catalog');
+    $('.container__slider').toggleClass('container__slider_catalog');
+
+    $('.main-section').toggleClass('main-section_black');
+    $(this).toggleClass('is-active');
+
+
+  });
+});
+
 
 /* eslint-enable */
